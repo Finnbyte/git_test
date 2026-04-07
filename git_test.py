c5689b408cbb21455604d9a6bce0cf64feb3751f
Eeli Laitinen <eeli.laitinen@edu.turkuamk.fi>
2026-04-07 08:39:45 +0300

    Added new line

--- git_test.py
+++ git_test.py
@@ -2,4 +2,4 @@ print("I am very familiar with Git! :)")
 
 print("Another line added")
 
-print("Yet another line added inside dev branch!")
+print("Yet another line added inside dev branch!")print("new line")
